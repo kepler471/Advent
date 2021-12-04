cd9f59e7addf039560ed1bc0671ed50c0cbfccf3
kepler471 <steliosdg@gmail.com>
2021-12-04 12:21:31 +0000

    part 2, final answer incorrect

--- 2021/03.fsx
+++ 2021/03.fsx
@@ -37,6 +37,41 @@ Use the binary numbers in your diagnostic report to calculate the gamma rate and
 Your puzzle answer was 1307354.
 
 The first half of this puzzle is complete! It provides one gold star: *
+--- Part Two ---
+
+Next, you should verify the life support rating, which can be determined by multiplying the oxygen generator rating by the CO2 scrubber rating.
+
+Both the oxygen generator rating and the CO2 scrubber rating are values that can be found in your diagnostic report - finding them is the tricky part. Both values are located using a similar process that involves filtering out values until only one remains. Before searching for either rating value, start with the full list of binary numbers from your diagnostic report and consider just the first bit of those numbers. Then:
+
+    Keep only numbers selected by the bit criteria for the type of rating value for which you are searching. Discard numbers which do not match the bit criteria.
+    If you only have one number left, stop; this is the rating value for which you are searching.
+    Otherwise, repeat the process, considering the next bit to the right.
+
+The bit criteria depends on which type of rating value you want to find:
+
+    To find oxygen generator rating, determine the most common value (0 or 1) in the current bit position, and keep only numbers with that bit in that position. If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+    To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit position, and keep only numbers with that bit in that position. If 0 and 1 are equally common, keep values with a 0 in the position being considered.
+
+For example, to determine the oxygen generator rating value using the same example diagnostic report from above:
+
+    Start with all 12 numbers and consider only the first bit of each number. There are more 1 bits (7) than 0 bits (5), so keep only the 7 numbers with a 1 in the first position: 11110, 10110, 10111, 10101, 11100, 10000, and 11001.
+    Then, consider the second bit of the 7 remaining numbers: there are more 0 bits (4) than 1 bits (3), so keep only the 4 numbers with a 0 in the second position: 10110, 10111, 10101, and 10000.
+    In the third position, three of the four numbers have a 1, so keep those three: 10110, 10111, and 10101.
+    In the fourth position, two of the three numbers have a 1, so keep those two: 10110 and 10111.
+    In the fifth position, there are an equal number of 0 bits and 1 bits (one each). So, to find the oxygen generator rating, keep the number with a 1 in that position: 10111.
+    As there is only one number left, stop; the oxygen generator rating is 10111, or 23 in decimal.
+
+Then, to determine the CO2 scrubber rating value from the same example above:
+
+    Start again with all 12 numbers and consider only the first bit of each number. There are fewer 0 bits (5) than 1 bits (7), so keep only the 5 numbers with a 0 in the first position: 00100, 01111, 00111, 00010, and 01010.
+    Then, consider the second bit of the 5 remaining numbers: there are fewer 1 bits (2) than 0 bits (3), so keep only the 2 numbers with a 1 in the second position: 01111 and 01010.
+    In the third position, there are an equal number of 0 bits and 1 bits (one each). So, to find the CO2 scrubber rating, keep the number with a 0 in that position: 01010.
+    As there is only one number left, stop; the CO2 scrubber rating is 01010, or 10 in decimal.
+
+Finally, to find the life support rating, multiply the oxygen generator rating (23) by the CO2 scrubber rating (10) to get 230.
+
+Use the binary numbers in your diagnostic report to calculate the oxygen generator rating and CO2 scrubber rating, then multiply them together. What is the life support rating of the submarine? (Be sure to represent your answer in decimal, not binary.)
+
 *)
 
 // Solve
@@ -46,7 +81,7 @@ let input =
     
 let diagnosticReport = 
     [ for i in input 
-        do yield [for c in i do yield int(c.ToString())]
+        do yield [for c in i do yield float(c.ToString())]
     ]
 
 let rec parseReport lines acc =
@@ -54,8 +89,64 @@ let rec parseReport lines acc =
     | [] -> acc
     | h :: t -> parseReport t (List.map2 (+) h acc)
 
-let epsilon = (parseReport diagnosticReport [for _ in 1..12 -> 0]) |> List.map (fun x -> float x /  float (List.length input))
+let epsilon = (parseReport diagnosticReport [for _ in 1..12 -> 0.0]) |> List.map (fun x -> float x /  float (List.length input))
 
 let gamma = epsilon |> List.map (fun x -> 1.0 - x)
 
-0b000101011101 * 0b111010100010
+let decToBin decimal bits =
+    [ for pow in bits..(-1)..0 do yield decimal >>> pow &&& 1]
+
+// Can this be done with a map2 or some kind of pairwise sequence?
+let binToDec binlist = 
+    let rec loop binlist pow acc =
+        match binlist with
+        | [] -> acc
+        | h :: t -> loop t (pow + 1) ((h <<< pow) + acc)
+    loop (List.rev binlist) 0 0
+
+let oxygen (matrix: float list list) =
+    let rec reduce (matrix: float list list) col =
+        match matrix with
+        | [row] -> row
+        | m -> 
+            let search = // are we looking for ones or zeros
+                let colAve = [ for i in m -> i.[col]] |> List.average // removed float on i.[col]
+                round colAve
+            let reduced = // filter by the search (1 or 0)
+                 m |> List.filter (fun f -> f.[col] = search)
+            reduce reduced (col + 1)
+    reduce matrix 0
+
+let co2 (matrix: float list list) =
+    let rec reduce (matrix: float list list) col =
+        match matrix with
+        | [row] -> row
+        | m -> 
+            let search = // are we looking for ones or zeros
+                let colAve = [ for i in m -> i.[col]] |> List.average // removed float on i.[col]
+                match round colAve with
+                | 0.0 -> 1.0
+                | 1.0 -> 0.0
+            let reduced = // filter by the search (1 or 0)
+                    m |> List.filter (fun f -> f.[col] = search)
+            reduce reduced (col + 1)
+    reduce matrix 0
+
+let answer = 
+    let a = oxygen diagnosticReport |> List.map (fun x -> int x) |> binToDec
+    let b = co2 diagnosticReport |> List.map (fun x -> int x) |> binToDec
+    a * b
+
+diagnosticReport |> List.filter (fun f -> f.[3] = 0) |> List.length
+
+printfn "%A" (binToDec [0;1;1;1])
+
+let a = [ for i in diagnosticReport -> float i.[3]] |> List.sum
+let b = diagnosticReport |> List.length |> float
+let c = round(a / b)
+
+// let abc = [for i in diagnosticReport -> i.[1]] |> List.sum 
+float 0.2
+
+[ for i in diagnosticReport -> float i.[3]] |> List.average |> round
+diagnosticReport
