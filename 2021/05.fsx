b2d4f205e3aace64660bf4c2ae8207361098efef
kepler471 <steliosdg@gmail.com>
2021-12-07 19:35:41 +0000

    day 5 done

--- 2021/05.fsx
+++ 2021/05.fsx
@@ -1,3 +1,79 @@
+(*
+--- Day 5: Hydrothermal Venture ---
+
+You come across a field of hydrothermal vents on the ocean floor! These vents constantly produce large, opaque clouds, so it would be best to avoid them if possible.
+
+They tend to form in lines; the submarine helpfully produces a list of nearby lines of vents (your puzzle input) for you to review. For example:
+
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+
+Each line of vents is given as a line segment in the format x1,y1 -> x2,y2 where x1,y1 are the coordinates of one end the line segment and x2,y2 are the coordinates of the other end. These line segments include the points at both ends. In other words:
+
+    An entry like 1,1 -> 1,3 covers points 1,1, 1,2, and 1,3.
+    An entry like 9,7 -> 7,7 covers points 9,7, 8,7, and 7,7.
+
+For now, only consider horizontal and vertical lines: lines where either x1 = x2 or y1 = y2.
+
+So, the horizontal and vertical lines from the above list would produce the following diagram:
+
+.......1..
+..1....1..
+..1....1..
+.......1..
+.112111211
+..........
+..........
+..........
+..........
+222111....
+
+In this diagram, the top left corner is 0,0 and the bottom right corner is 9,9. Each position is shown as the number of lines which cover that point or . if no line covers that point. The top-left pair of 1s, for example, comes from 2,2 -> 2,1; the very bottom row is formed by the overlapping lines 0,9 -> 5,9 and 0,9 -> 2,9.
+
+To avoid the most dangerous areas, you need to determine the number of points where at least two lines overlap. In the above example, this is anywhere in the diagram with a 2 or larger - a total of 5 points.
+
+Consider only horizontal and vertical lines. At how many points do at least two lines overlap?
+
+Your puzzle answer was 8060.
+--- Part Two ---
+
+Unfortunately, considering only horizontal and vertical lines doesn't give you the full picture; you need to also consider diagonal lines.
+
+Because of the limits of the hydrothermal vent mapping system, the lines in your list will only ever be horizontal, vertical, or a diagonal line at exactly 45 degrees. In other words:
+
+    An entry like 1,1 -> 3,3 covers points 1,1, 2,2, and 3,3.
+    An entry like 9,7 -> 7,9 covers points 9,7, 8,8, and 7,9.
+
+Considering all lines from the above example would now produce the following diagram:
+
+1.1....11.
+.111...2..
+..2.1.111.
+...1.2.2..
+.112313211
+...1.2....
+..1...1...
+.1.....1..
+1.......1.
+222111....
+
+You still need to determine the number of points where at least two lines overlap. In the above example, this is still anywhere in the diagram with a 2 or larger - now a total of 12 points.
+
+Consider all of the lines. At how many points do at least two lines overlap?
+
+Your puzzle answer was 21577.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+*)
+
 let input = 
     "inputs/05.txt" 
     |> System.IO.File.ReadLines 
@@ -5,163 +81,40 @@ let input =
     |> Seq.map (fun x -> x.Split [|','|])
     |> Seq.map (fun x -> [|for i in x -> int i|])
 
-let removeDiagonal (lines: int [] seq) =
-    lines
-    |> Seq.filter (fun x -> x.[0] = x.[2] || x.[1] = x.[3])
-
-let generateGrid height width = 
-    Array2D.create height width 0
-
-let (.+) a b = 
-    Array2D.map (fun x -> x + b) a
-
-let buildOnGrid (l: int []) (g: int [,]) = 
-    g.[l.[0]..l.[2],l.[1]..l.[3]] <- g.[l.[0]..l.[2],l.[1]..l.[3]] .+ 1
-    g
-
 let parseLine (line: int []) = 
-    if line.[0] > line.[2]
-    then (line.[0], line.[1]), (line.[2], line.[3])
-    elif line.[1] > line.[3]
-    then (line.[0], line.[1]), (line.[2], line.[3])
-    else (line.[0], line.[1]), (line.[2], line.[3])
+    (line.[0], line.[1]), (line.[2], line.[3])
 
-input |> Seq.map parseLine
+let isStraight (a, b) = 
+    fst a = fst b || snd a = snd b
 
-let x = fst
-let y = snd
-// Instead of marking matrix, count repeated coordinates of all lines?
 let buildLine (a, b) =
     let (ax, ay), (bx, by) = (a, b)
     let inc = 
         match ax, ay, bx, by with
         | _ when ax > bx && ay > by -> (-1, -1)
-        | _ when ax > bx && ay < by -> (-1,  1)
-        | _ when ax < bx && ay > by -> (1,  -1)
+        | _ when ax > bx && ay <= by -> (-1,  1)
+        | _ when ax <= bx && ay > by -> (1,  -1)
         | _ -> (1,  1)
 
-    [| for i in x(a)..y(b) do
-            for j in snd a..snd b do yield (i,j) |]
+    if isStraight (a,b)
+    then seq { for i in ax..(fst inc)..bx do
+                    for j in ay..(snd inc)..by do yield (i,j) }
+    else Seq.zip [ax..(fst inc)..bx] [ay..(snd inc)..by]
 
+let flatten s = 
+    seq { for i in s do
+            for j in i do yield j }
 
-
-
-let buildLine2 (l: int []) =
-    [| for i in l.[0]..l.[2] do
-            for j in l.[0]..l.[2] do yield (i,j) |]
-
-let removeDiagonalLines lines = 
-    lines |> Seq.filter (fun (a, b) -> fst a = fst b || snd a = snd b)
-
-let newans = 
+let countOverlaps minOverlap lineType input  = 
     input 
-    |> Seq.map parseLine
-    |> removeDiagonalLines 
-    // |> Seq.map (fun (a, b) -> buildLine (a, b))
+    |> Seq.map parseLine 
+    |> Seq.filter lineType
+    |> Seq.map buildLine
+    |> flatten
+    |> Seq.countBy id 
+    |> Seq.filter (fun (_, b) -> b >= 2) 
+    |> Seq.length
 
-newans
-seq { for i in newans do yield buildLine i}
-((657,934),(657,926)) |> buildLine
-//  [((657, 934), (657, 926));
-input |> removeDiagonal
-input |> Seq.map parseLine |> Seq.head
-input |> Seq.map parseLine |> Seq.filter (fun (a, b) -> fst a = fst b || snd a = snd b)
-input |> Seq.map parseLine |> Seq.filter (fun (a, b) -> fst a = 657)
-let x,y = (input |> Seq.map parseLine |> Seq.head)
-fst x = fst y
-buildLine (1,1) (3,1)
-
-let x lines = 
-    let g = generateGrid 1000 1000
-    let rec go lines grid = 
-        match lines with
-        | [] -> grid
-        | h :: t -> 
-            go t (buildOnGrid h grid)
-    go (Seq.toList lines) g
-
-let ans = input |> removeDiagonal |> x
-
-let flat2Darray array2D = 
-                seq { for x in [0..(Array2D.length1 array2D) - 1] do 
-                          for y in [0..(Array2D.length2 array2D) - 1] do 
-                              yield array2D.[x, y] }
-
-flat2Darray ans |> Seq.map (fun x -> if x >= 2 then 1 else 0) |>  Seq.sum
-flat2Darray newans
-
-seq { for i in 0..999 do 
-        for j in 0..999 do 
-            yield if ans.[100,j] >= 2 then 1 else 0
-} |> Seq.sum
-
-let ans2 = 
-    let mutable count = 0
-    for i in 0..0999 do
-        for j in 0..999 do
-            if ans.[i,j] >= 2 then count <- count + 1
-    count
-
-
-let g = generateGrid 10 10
-g.[0..2, 0..2] |> Array2D.map (fun x -> x + 2)
-g.[0..2, 0..2] <- g.[0..2, 0..2] .+ 2
-g
-g
-let testin = (input |> Seq.head)
-let testin2 = [|1;1;1;8|]
-buildOnGrid testin g
-g.[5..5,0..9] |> Array2D.mapi (fun i j x -> Array2D.set g (i+4) j 69) 
-g.[5..5,0..9] <- g.[5..5,0..9] .+ 101
-g.[1,8] + 222
-g
-
-let testinput = [|0;0;0;999|]::Seq.toList input |> List.toSeq
-testinput
-let ans = x testinput
-let ans = x input |> Array2D.iter (fun x -> if x <> 0 then printfn "%A" x)
-
-g.[1..9,9..9] <- g.[1..9,9..9] .+ 23
-g
-Array2D.set
-// "123,456 -> 234,345".Replace(" -> ", ",")
-// // let isHorizontal (a, b) = 
-// g.[0..4,2] <- Array.create 5 1
-// Array2D.create 1 5 1
-// Array.create 5 1
-// "1234".[1]
-// g
-// // g.[0..4,2] <- g.[0..4,2] .+ 1
-// g
-
-//     //Array.create (l.[2]-l.[0]) (l.[3]-l.[1]) 1
-// // let (.+) (a: int [], b: int) = 
-// //     a |> Array.map (fun x -> x + b)
-
-
-
-// [|1;2;3;4|] .+ 2
-// g.[0..2, 0..2] <- g.[0..2, 0..2] .+ Array2D.create 3 3 1
-// // 4l.GetType()
-
-// g.[0..2, 2..4] |> Array2D.map (fun x -> x + 1)
-// let (.+) (a: int) (b: int []) = 
-//     Array.map (fun x -> x + a) b
-
-// Array.zeroCreate 10
-
-// type Array = Array with
-//     static member inline (.+) ((a: int []), (b: int)) = fun Array.map (fun x -> x + b) a
-//     static member inline (.+) ((a: int), (b: int [])) = Array.map (fun x -> x + a) b
-
-
-// type Array = Array with
-//     static member inline (.+) (a: int [], b: int) = fun (b: int   ) -> a |> Array.map (fun x -> x + b)
-//     static member inline (.+) (a: int, b: int []) = fun (b: int []) -> b |> Array.map (fun x -> x + a)
-
-
-
-// let inline (.+) (a: int, b: int []) = 
-//     b |> Array.map (fun x -> x + a)
-
-// (.+)
+printfn "--- Day 5: Hydrothermal Venture ---"
+printfn "Part One: -> %A" (countOverlaps 2 isStraight input)
+printfn "Part Two: -> %A" (countOverlaps 2 (fun _ -> true) input)
