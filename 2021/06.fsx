0abfd66a388e4394fffe8b80b838322fa9cce66e
kepler471 <steliosdg@gmail.com>
2021-12-08 01:04:40 +0000

    new mapping of pop for aging one day

--- 2021/06.fsx
+++ 2021/06.fsx
@@ -24,8 +24,8 @@ let iterate2 n f =
     let rec iteratetail n f acc =
         match n with
         | 0 -> id
-        | _ -> iteratetail (n-1) f (f >> iterate (n-1) f)
-    iteratetail n f f
+        | _ -> iteratetail (n-1) f (f >> iteratetail (n-1) f)
+    iteratetail n f id
 
 let rec iterage n fishTimers =
     match n with
@@ -35,8 +35,8 @@ let rec iterage n fishTimers =
 let flatten s = 
     seq { for i in s do
             for j in i do yield j }
-// ((iterate 110) age input) |> List.length
 
+((iterate 110) age input)
 iterage 80 [3] |> Seq.length
 ((iterate 80) age input) |> Seq.length
 
@@ -76,8 +76,44 @@ Seq.zip (finalMults |> Seq.map snd) finalEpoch
     |> Seq.map (fun (x, y) -> uint64 x * uint64 y)
     |> Seq.sum
 
+/// Try making a population type that mirrors the data type 
+///     from the output of Seq.countBy
+// type population = 
+//     {
+//          : int 
+//         1: int 
+//     }
 
+/// epoch maps the (i * n), i = 0..8 description of a population
+///     from one epoch to the next. 
+let epoch (i: (int * int) seq) = 
+    let [
+            (i0, n0)
+            (i1, n1)
+            (i2, n2)
+            (i3, n3)
+            (i4, n4)
+            (i5, n5)
+            (i6, n6)
+            (i7, n7)
+            (i8, n8)
+        ] = i |> Seq.toList
+    [
+        (i0, n1)
+        (i1, n2)
+        (i2, n3)
+        (i3, n4)
+        (i4, n5)
+        (i5, n6 + n0)
+        (i6, n7)
+        (i7, n8)
+        (i8, n0)
+    ] |> List.toSeq
+    
 
+finalMults
+epoch finalMults
+((iterate 128) epoch) finalMults
 
 // let ans2 = iterage 256 input |> List.length
 // ((iterate 80) age [3;4;3;1;2]) |> List.length
@@ -88,3 +124,4 @@ Seq.zip (finalMults |> Seq.map snd) finalEpoch
 // let square n = n * n
 // input |> List.length
 // ((iterate 2) square) 2
+((iterate 128) epoch) (input |> Seq.countBy id)
