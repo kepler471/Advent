42a753007a36f10f1982b97f1313ef8b79ac47d9
Stelios Georgiou <steliosdg@gmail.com>
2022-12-23 09:32:17 +0000

    add notes

--- 2022/21.py
+++ 2022/21.py
@@ -11,9 +11,9 @@ def parse(line):
             return "exp", name.strip(":"), equation
 
 
-def yelling(num, root_op):
-    values = {d[1]: d[2] for d in data if d[0] == "val"}
-    expressions = {d[1]: d[2] for d in data if d[0] == "exp"}
+def yelling(monkey_calls, num, root_op):
+    values = {d[1]: d[2] for d in monkey_calls if d[0] == "val"}
+    expressions = {d[1]: d[2] for d in monkey_calls if d[0] == "exp"}
     expressions["root"] = [expressions["root"][0], root_op, expressions["root"][2]]
     values["humn"] = num
     while expressions:
@@ -30,7 +30,16 @@ def yelling(num, root_op):
 
 
 data = lmap(parse, read_input(21, 2022, test=False).splitlines())
+print("root yells: ", int(yelling(data, [d[2] for d in data if d[1] == "humn"][0], "+")))
+
 # The only variable is "humn", so this can be solved with linear regression
 model = LinearRegression()
-model.fit(x := np.linspace(1, 10e12, 101).reshape((-1, 1)), y := yelling(x, "-"))
+
+# Rather than letting root compare equality on two numbers, take their difference.
+# The difference would be zero when the numbers are equal.
+# let y(x) = m * x + c, where y is a function that for any given x, calculates this
+#  difference of the numbers at root.
+# We require y = n1 - n2 = 0, so y = 0 = m * x_0 + c  -->  x_0 = - c / m
+model.fit(x := np.linspace(1, 10e12, 101).reshape((-1, 1)), y := yelling(data, x, "-"))
+
 print("Yell the number: ", int(x_0 := - model.intercept_ / model.coef_))
