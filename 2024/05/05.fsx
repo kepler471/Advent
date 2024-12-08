fd233e8b455ea96ba3792fdc5d247a8288f3de07
Stelios Georgiou <steliosdg@gmail.com>
2024-12-08 19:16:19 +0000

    moved findCharts to utils.fs

--- 2024/05/05.fsx
+++ 2024/05/05.fsx
@@ -4,14 +4,6 @@ open _2024.utils
 
 let areaMap = System.IO.File.ReadLines("2024/05/input") |> array2D
 
-let findChars (char: char) (arr: char array2d) =
-    arr
-    |> Array2D.mapi (fun i j c -> if c = char then Some(i, j) else None)
-    |> Seq.cast<Option<int * int>> // Converts the 2D array to a sequence of options
-    |> Seq.filter Option.isSome
-    |> Seq.map Option.get
-    |> Seq.toList
-
 let findGuard map = findChars '^' map |> List.head
 let start = findGuard areaMap
 let obstacles = findChars '#' areaMap
@@ -41,34 +33,78 @@ printfn $"Part One: %d{0}"
 printfn $"Part Two: %d{0}"
 
 let rotateDirs = rotate dirs Clockwise
+let lookUpTo view item = view |> Array.takeWhile ((<>) item)
+let findFirstCrossing oldPaths newPath = ()
+    
+let rec moveGuard pos dir map visits =
+    let view = look dir map (fst pos, snd pos)
 
+    match view with
+    | arr when Array.head arr = '#' ->
+        // guard is collided with an obstacle
+        moveGuard pos (rotateDirs dir) map visits
+    | arr when not (Array.contains '#' arr) ->
+        // guard sees no obstacles so will exit the map
+        let path = [ for x in 0 .. view.Length -> (dir, move dir x pos) ]
+        
+        // Check for obstacle placements
+        // findFirstCrossing
+        
+        (visits @ [path])
+    | _ ->
+        // guard move towards an obstacle
+        let pathItems = view |> Array.takeWhile ((<>) '#')
+        // let pathItems = lookUpTo view '#'
+        let newPos = move dir pathItems.Length pos
+        let path = [ for x in 0 .. pathItems.Length -> (dir, move dir x pos) ]
+        
+        moveGuard newPos dir map (visits @ [path])
 
 // This version of the simulation does not update the map as it plays out
 let simulate startPos map =
-    let rec moveGuard pos dir visits =
-        let view = look dir map (fst pos, snd pos)
-
-        match view with
-        | arr when Array.head arr = '#' ->
-            // guard is collided with an obstacle
-            moveGuard pos (rotateDirs dir) visits
-        | arr when not (Array.contains '#' arr) ->
-            // guard sees no obstacles so will exit the map
-            let path = [ for x in 0 .. view.Length -> move dir x pos ]
-            (visits @ path)
-        | _ ->
-            // guard move towards an obstacle
-            let pathItems = view |> Array.takeWhile ((<>) '#')
-            let newPos = move dir pathItems.Length pos
-            let path = [ for x in 0 .. pathItems.Length -> move dir x pos ]
-            moveGuard newPos dir (visits @ path)
-
     let iInit, jInit = findGuard map
     let dirInit = getDir map[iInit, jInit]
-    moveGuard startPos dirInit [ startPos ]
+    moveGuard startPos dirInit map [[]]
+    
+    // let rec moveGuardStepwise pos dir map visits =
+    //     let view = look dir map (fst pos, snd pos) |> List.ofArray
+    //
+    //     match view with
+    //     | '#' :: t ->
+    //         // guard is collided with an obstacle
+    //         map[fst pos, snd pos] <- '.'
+    //         moveGuardStepwise pos (rotateDirs dir) map visits
+    //     | [ '.' ]->
+    //         // guard has reached the boundary of the map
+    //         let newPos = move dir 1 pos
+    //         newPos :: visits
+    //     | '.' :: t ->
+    //         // spawn alternate universe simulation
+    //         // if alt-sim loops, log the position
+    //         // let loop = spawnSim
+    //         
+    //         // guard moving freely
+    //         let newChar = if dir = Up || dir = Do then '|' else '-'
+    //         let newPos = move dir 1 pos
+    //         map[fst newPos, snd newPos] <- newChar
+    //         moveGuardStepwise newPos dir map (newPos :: visits)
+    //     // | '-' :: t ->
+    //         
+    //     // | '|' :: t ->
+    //     // | '+' :: t ->
+    //         // guard has come across a previously traversed location
+    //         // 
+    //     | _ :: t -> failwith "Should not have reached this case. Other char type"
+    //     | [] -> failwith "Should not have reached this case. Empty list"
+    // moveGuardStepwise startPos dirInit areaMap [ startPos ]
 
-simulate start areaMap |> Set |> Set.toList |> List.length
-//
+let flatPath = simulate start areaMap |> List.concat
+flatPath |> List.map snd |> Set |> Set.toList |> List.length
+simulate start areaMap |> List.length
+
+
+
+
 // not (Array.contains '#' [|'.'|])
 //
 // look Up areaMap start
