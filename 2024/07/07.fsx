89fe0eff6bf6c1962d37b0272a44572f44089cb7
Stelios Georgiou <steliosdg@gmail.com>
2024-12-08 20:58:54 +0000

    complete

---
+++ 2024/07/07.fsx
@@ -0,0 +1,56 @@
+#load "../utils.fs"
+
+open Microsoft.FSharp.Core
+open _2024.utils
+open System
+
+type Equation =
+    { Result: int64 
+      Parts: int64 list }
+    
+    static member parseEquation (input: string) : Equation =
+        let parts = 
+            input.Replace(":", "").Split([|' '|], StringSplitOptions.RemoveEmptyEntries)
+            |> Array.map int64
+        { Result = parts[0]; Parts = parts[1..] |> List.ofArray}
+        
+let equations = System.IO.File.ReadAllLines("2024/07/input") |> Seq.map Equation.parseEquation
+
+let ops1 = [ (+); (*) ]
+let ( |~| ) (a: int64) (b: Int64) = string a + string b |> int64
+let ops2 = ( |~| ) :: ops1 
+
+let rec permutationsWithReplacement n list =
+    if n = 0 then
+        [[]]
+    else
+        list
+        |> List.collect (fun x ->
+            permutationsWithReplacement (n - 1) list
+            |> List.map (fun perm -> x :: perm)
+        )
+
+let rec burn nums (ops: (int64 -> int64 -> int64) list) =
+    match nums with
+    | a :: b :: t -> burn (ops.Head a b :: t) ops.Tail 
+    | [ x ] -> x
+    | [] -> failwith "todo"
+
+let trySolve (ops: (int64 -> int64 -> int64) list) (eq: Equation) =
+    let nOps = eq.Parts.Length - 1
+    let opCombs = permutationsWithReplacement nOps ops
+    seq {
+        for opComb in opCombs ->
+            let res = burn eq.Parts opComb
+            if eq.Result = res then Some(res) else None
+    } |> Seq.tryPick id
+
+let calculateCalibrationResult equations ops =
+    equations
+    |> Seq.map (trySolve ops)
+    |> Seq.filter Option.isSome
+    |> Seq.map _.Value
+    |> Seq.sum
+
+printfn $"Part One: %d{calculateCalibrationResult equations ops1}"
+printfn $"Part Two: %d{calculateCalibrationResult equations ops2}"
