15b9133f032902dc678303cac6040a3b8461d6bc
Stelios Georgiou <steliosdg@gmail.com>
2024-12-15 23:33:56 +0000

    complete

--- 2024/11/11.fsx
+++ 2024/11/11.fsx
@@ -1,8 +1,11 @@
-open System.Collections.Generic
 open Microsoft.FSharp.Core
 
-let test = [| "125"; "17" |]
-let plutoPebbles = System.IO.File.ReadAllText("2024/11/input").Split(" ")
+let testPebbles =
+    [| "125"; "17" |] |> Seq.countBy id |> Seq.map (fun (s, n) -> s, uint64 n)
+let plutoPebbles =
+    System.IO.File.ReadAllText("2024/11/input").Split(" ")
+    |> Seq.countBy id
+    |> Seq.map (fun (s, n) -> s, uint64 n)
 
 let fn1 (stone: string) = [| "1" |]
 let fn2 (stone: string) =
@@ -16,34 +19,16 @@ let decideRule stone =
     | _ when stone.Length % 2 = 0 -> fn2 stone
     | _ -> fn3 stone
 
-let blink stones=
-    stones |> Array.collect decideRule
-
 let iterN n fn =
     let rec iter fn' n' = if n' = 0 then fn' else iter (fn >> fn') (n' - 1)
     iter id n
 
-    
-iterN 1 blink test
-iterN 2 blink test
-iterN 3 blink test
-iterN 4 blink test
-iterN 5 blink test
-iterN 6 blink test
-iterN 6 blink test |> Seq.length
-iterN 25 blink test |> Seq.length
-iterN 25 blink plutoPebbles |> Seq.length
-iterN 75 blink plutoPebbles
+let blink' (pebbles: (string * uint64) seq)=
+    pebbles
+    |> Seq.map (fun (str, n) -> decideRule str |> Array.map (fun str -> str, n))
+    |> Seq.collect id
+    |> Seq.groupBy fst
+    |> Seq.map (fun (k, v) -> k, v |> Seq.sumBy snd)
 
-    
-let rec counter (x: 'a seq) (acc: uint64)=
-    match x with
-    | _ when Seq.isEmpty x -> acc
-    | _ -> counter (Seq.tail x) (acc + 1UL)
-
-counter (iterN 25 blink plutoPebbles) 0UL
-
-(iterN 11 blink plutoPebbles) |> Seq.length
-Seq.empty
-counter (iterN 25 blink plutoPebbles)
-
+iterN 25 blink' plutoPebbles |> Seq.sumBy snd
+iterN 75 blink' plutoPebbles |> Seq.sumBy snd
