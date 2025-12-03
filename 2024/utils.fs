72c13345fc4c84f30be204a9b74a2d57bd0763f3
Stelios Georgiou <steliosdg@gmail.com>
2025-12-03 23:48:04 +0000

    day 3 donezo!

--- 2024/utils.fs
+++ 2024/utils.fs
@@ -1,5 +1,25 @@
 module _2024.utils
 
+open System
+open FSharp
+// open FSharpAux
+// open FSharpAux
+// open Plotly.NET
+open System.Collections.Generic
+open System.Collections.Concurrent
+open System.Text.RegularExpressions
+
+
+let memoise f =
+    let cache = ConcurrentDictionary()
+    fun x -> cache.GetOrAdd(x, lazy f x).Value
+
+let memoiseRec f =
+    let cache = ConcurrentDictionary()
+    let rec f' x = cache.GetOrAdd(x, lazy (f f' x)).Value
+    f'
+
+
 module Input =
     let paragraphs file = System.IO.File.ReadAllText(file).Split("\n\n")
     let lines file = System.IO.File.ReadAllLines(file)
@@ -14,6 +34,32 @@ type Point =
     static member (*) (p: Point, scalar: int) = { x = p.x * scalar; y = p.y * scalar }
     static member (*) (scalar: int, p: Point) = { x = p.x * scalar; y = p.y * scalar }
 
+type RingBuffer2D<'T>(items : 'T[,]) =
+    let leni = items.GetLength(0)
+    let lenj = items.GetLength(1)
+    let _items = Array2D.copy items
+    member _.Item
+        with get(i, j) =
+            _items.[i % leni, j % lenj]
+        and set (i, j) value =
+            _items.[i % leni, j % lenj] <- value
+            
+let numbers = Array2D.init 4 5 (fun x y -> x * y)
+let numberRing = RingBuffer2D(numbers)
+
+
+// 0 0 -> 0
+// 0 1 -> 0
+// ...
+// 1 1 -> 1
+// 1 2 -> 2
+// ..
+// 9 8 -> 3
+// 9 9 -> 4
+for i in 0..9 do
+    for j in 0..9 do
+        printfn "%i %i -> %A" i j (numberRing.[i,j])
+
 let manhattan a b =
     abs(a.x - b.x) + abs(a.y - b.y)
 
@@ -21,6 +67,10 @@ let rec rep n (item: string) =
     match n with
     | 0 -> item
     | _ -> rep (n - 1) (item + item)
+
+let repStr (str: string) (n: int) : string =
+    if n <= 0 then ""
+    else String.replicate n str
 
 let iterN n fn =
     let rec iter fn' n' = if n' = 0 then fn' else iter (fn >> fn') (n' - 1)
@@ -122,7 +172,7 @@ let inbounds (map: char array2d) (pos: Point) =
     pos.x >= map.GetLowerBound 0 && pos.x <= map.GetUpperBound 0 &&
     pos.y >= map.GetLowerBound 0 && pos.y <= map.GetUpperBound 0
 
-let printCharMap (map: char array2d) spacing =
+let printCharMap spacing (map: char array2d) =
     printf "  "
     for col in 0 .. Array2D.length2 map - 1 do
         let spacing' = rep spacing " "
@@ -135,7 +185,7 @@ let printCharMap (map: char array2d) spacing =
             printf $"%c{map[row, col]}{spacing'}"
         printfn ""
 
-let printIntMap (map: int array2d) spacing =
+let printIntMap spacing (map: int array2d) =
     for row in 0 .. Array2D.length1 map - 1 do
         for col in 0 .. Array2D.length2 map - 1 do
             let spacing' = rep spacing " "
@@ -149,36 +199,181 @@ let rec permutationsWithReplacement n list =
         list
         |> List.collect (fun x -> permutationsWithReplacement (n - 1) list |> List.map (fun perm -> x :: perm))
 
-let aStar (grid: char array2d) (start: Point) (goal: Point) (heuristic: Point -> Point -> int) =
-    let frontier = PriorityQueue<Point * Dirs, int>()
-    frontier.Enqueue((start, Ri), 0)
-    let from = Dictionary<Point, Point option>()
-    let cost = Dictionary<Point, int>()
-    from.Add(start, None)
-    cost.Add(start, 0)
-    let mutable Break = false
+module Search =
+    let fillRegion (map: char array2d) (start: Point) (fn: Point -> char array2d -> Point-> bool): Point seq =
+        let visited = HashSet<Point>()
     
-    while frontier.Count > 0 && not Break do
-        let current, facing = frontier.Dequeue()
+        let rec step (pos: Point) =
+            seq {
+                if visited.Contains(pos) then
+                    ()
+                else
+                    visited.Add(pos) |> ignore
+                    yield pos
+    
+                    let neighbours =
+                        dirs
+                        |> List.map (fun dir -> move dir 1 pos)
+                        |> List.filter (fn pos map)
+    
+                    for neighbour in neighbours do
+                        yield! step neighbour
+            }
+    
+        step start
+
+    let dijkstra (grid: char array2d) (start: Point) (goal: Point)=
+        let frontier = PriorityQueue<Point * Dirs, int>()
+        frontier.Enqueue((start, Ri), 0)
+        let from = Dictionary<Point, Point option>()
+        let cost = Dictionary<Point, int>()
+        from.Add(start, None)
+        cost.Add(start, 0)
+        let mutable Break = false
         
-        if current = goal then
-            printfn "REACHED THE BLOODY GOAL INNIT"
-            cost[goal] <- cost[current] + 1
-            from[goal] <- Some(current)
-            Break <- true
-        else
-            let options = dirs |> List.filter (fun dir -> charAt (move dir 1 current) grid = '.')
-            let neighbours = options |> List.map (fun dir -> move dir 1 current)
+        while frontier.Count > 0 && not Break do
+            let current, facing = frontier.Dequeue()
             
-            for next, dir in List.zip neighbours options do
-                let turnCost = if dir = facing then 0 else 1000
-                let cost' = cost[current] + 1 + turnCost
-                if cost.ContainsKey(next) |> not || cost' <= cost[next] then
-                    cost[next] <- cost'
-                    let priority = cost' + heuristic next goal
-                    frontier.Enqueue((next, dir), priority)
-                    from[next] <- Some(current)
-    from, cost  
+            if current = goal then
+                printfn "REACHED THE BLOODY GOAL INNIT"
+                cost[goal] <- cost[current] + 1
+                from[goal] <- Some(current)
+                Break <- true
+            else
+                let options = dirs |> List.filter (fun dir -> charAt (move dir 1 current) grid = '.')
+                let neighbours = options |> List.map (fun dir -> move dir 1 current)
+                
+                for next, dir in List.zip neighbours options do
+                    let turnCost = if dir = facing then 0 else 1000
+                    let cost' = cost[current] + 1 + turnCost
+                    if cost.ContainsKey(next) |> not || cost' <= cost[next] then
+                        cost[next] <- cost'
+                        let priority = cost'
+                        frontier.Enqueue((next, dir), priority)
+                        from[next] <- Some(current)
+        from, cost
+    
+    let aStar (grid: char array2d) (start: Point) (goal: Point) (heuristic: Point -> Point -> int) =
+        let frontier = PriorityQueue<Point * Dirs, int>()
+        frontier.Enqueue((start, Ri), 0)
+        let from = Dictionary<Point, Point option>()
+        let cost = Dictionary<Point, int>()
+        from.Add(start, None)
+        cost.Add(start, 0)
+        let mutable Break = false
+        
+        while frontier.Count > 0 && not Break do
+            let current, facing = frontier.Dequeue()
+            
+            if current = goal then
+                printfn "REACHED THE BLOODY GOAL INNIT"
+                cost[goal] <- cost[current] + 1
+                from[goal] <- Some(current)
+                Break <- true
+            else
+                let options = dirs |> List.filter (fun dir -> charAt (move dir 1 current) grid = '.')
+                let neighbours = options |> List.map (fun dir -> move dir 1 current)
+                
+                for next, dir in List.zip neighbours options do
+                    let turnCost = if dir = facing then 0 else 1000
+                    let cost' = cost[current] + 1 + turnCost
+                    if cost.ContainsKey(next) |> not || cost' <= cost[next] then
+                        cost[next] <- cost'
+                        let priority = cost' + heuristic next goal
+                        frontier.Enqueue((next, dir), priority)
+                        from[next] <- Some(current)
+        from, cost
+        
+    let traverse (map: char array2d) (start: Point) (fn: Point -> char array2d -> Point-> bool)=
+        let visited = HashSet<Point>()
+
+        let rec loop pos path = seq {
+            let height = digitAt pos map
+            let path' = (pos, height) :: path
+            visited.Add(pos) |> ignore
+            
+            if height = 9 then yield path'
+            
+            let neighbours =
+                dirs
+                |> List.map (fun dir -> move dir 1 pos)
+                |> List.filter (fun pos' -> inbounds map pos' && fn pos map pos')
+
+            for neighbor in neighbours do
+                yield! loop neighbor path'
+        }
+
+        loop start []
+        
+    let allPaths branchCond stopCond map start  =
+        let rec dfs (pos: Point) (visited: Point Set) : Point list seq =
+            let visited' = visited.Add(pos)
+
+            let neighbours =
+                dirs
+                |> List.map (fun dir -> move dir 1 pos)
+                |> List.filter (fun pos' -> branchCond pos map pos' && visited.Contains(pos') |> not)
+
+            if neighbours.IsEmpty || stopCond pos map then
+                // end of a path
+                seq { yield [ pos ] }
+            else
+                neighbours
+                |> Seq.collect (fun n -> dfs n visited' |> Seq.map (fun path -> pos :: path))
+
+        dfs start Set.empty        
+    
+    let allPaths16 branchCond stopCond costFn map start state  =
+        let rec dfs (pos: Point) (visited: Point Set) cost state =
+            let visited' = visited.Add(pos)
+
+            let neighbours =
+                dirs
+                |> List.map (fun dir -> dir, move dir 1 pos)
+                |> List.filter (fun (_, pos') -> branchCond pos map pos' && visited.Contains(pos') |> not)
+
+            if neighbours.IsEmpty || stopCond pos map visited then
+                // end of a path
+                seq { yield ([ pos ], cost) }
+            else
+                neighbours
+                |> Seq.collect (fun (dir, pos') ->
+                    let cost' = cost + costFn pos pos' state dir
+                    (dfs pos' visited' cost' dir)
+                    |> Seq.map (fun (path, cost) -> (pos :: path, cost)))
+
+        dfs start Set.empty 0 state
+    
+    let aStar18 (grid: char array2d) (start: Point) (goal: Point) (heuristic: Point -> Point -> int) =
+        // TODO: add branch, stop, and cost functions as args, then move to utils
+        let frontier = PriorityQueue<Point, int>()
+        frontier.Enqueue(start, 0)
+        let from = Dictionary<Point, Point option>()
+        let cost = Dictionary<Point, int>()
+        from.Add(start, None)
+        cost.Add(start, 0)
+        let mutable Break = false
+        
+        while frontier.Count > 0 && not Break do
+            let pos = frontier.Dequeue()
+            
+            if pos = goal then
+                printfn "REACHED THE BLOODY GOAL INNIT"
+                Break <- true
+            else
+                let neighbours =
+                    dirs
+                    |> List.map (fun dir -> dir, move dir 1 pos)
+                    |> List.filter (fun (_, pos') -> inbounds grid pos' && charAt pos' grid = '.')
+
+                for _, pos' in neighbours do
+                    let cost' = cost[pos] + 1
+                    if cost.ContainsKey(pos') |> not || cost' < cost[pos'] then
+                        cost[pos'] <- cost'
+                        let priority = cost' + heuristic pos' goal
+                        frontier.Enqueue(pos', priority)
+                        from[pos'] <- Some(pos)
+        from, cost
 
 type Matrix<'T>(N: int, M: int) =
     let internalArray = Array2D.zeroCreate<'T> N M
@@ -277,3 +472,33 @@ let findIndicesOf (char: char) (arr: char[,]) =
 
 let findXs = findIndicesOf 'X'
 let findMs = findIndicesOf 'M'
+
+module Validation =
+    let showWarning message =
+        let originalColor = System.Console.ForegroundColor
+        System.Console.ForegroundColor <- System.ConsoleColor.Yellow
+        printfn $"WARNING: %s{message}"
+        System.Console.ForegroundColor <- originalColor
+
+    let ensure statement =
+        if statement then () else showWarning "ensure failed"
+        
+    ensure (9 = 0)
+
+    open Microsoft.FSharp.Quotations
+    open Microsoft.FSharp.Linq.RuntimeHelpers
+
+    /// The ensure function
+    let ensure2 (expr: bool Expr) =
+        // Evaluate the expression
+        let isTrue = LeafExpressionConverter.EvaluateQuotation expr :?> bool
+        if not isTrue then
+            // Get the textual representation of the expression
+            let exprString = expr.ToString()
+            printfn "Warning: Ensure failed for expression: %s" exprString
+
+    // Example usage
+    let x = 5
+    let y = 10
+    ensure2 <@ x + y = 20 @>  // Outputs: Warning: Ensure failed for expression: x + y = 20
+    ensure2 <@ x < y @>       // No output because the condition is true
--- 2025/03/03.fsx
+++ 2025/03/03.fsx
@@ -3,3 +3,23 @@
 open _2025.utils 
 open System.Collections.Generic
 
+let banks = Input.lines "03/input" |> Seq.map (Seq.map digitToInt)
+
+// Look from the right of the bank to the left
+// Make the function flexible for the number of batteries
+// Reverse the bank
+// For n batteries, look for b_1 by skipping n - 1 batteries
+// ... and so on
+let maxJoltage (digits: int) (bank: int seq) =
+    let rec findMaxNum (search: int seq) (stack: int list) (n: int) =
+        if n = 0 then
+            stack |> List.rev |> List.map int64 |> digitsToInt64 
+        else
+            let largestBattery = search |> Seq.rev |> Seq.skip (n - 1) |> Seq.max
+            let batteryLocation = search |> Seq.findIndex ((=) largestBattery)
+            findMaxNum (Seq.skip (batteryLocation + 1) search) (largestBattery :: stack) (n - 1)
+
+    findMaxNum bank [] digits
+
+banks |> Seq.map (maxJoltage 2) |> Seq.sum
+banks |> Seq.map (maxJoltage 12) |> Seq.sum
